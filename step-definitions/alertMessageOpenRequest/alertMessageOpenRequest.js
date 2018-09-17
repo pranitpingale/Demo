a5cd555679211eb8d7adb7b2a5f2edca06e7d1c9
gourav24 <gourav.kumar@accionlabs.com>
2018-09-17 09:31:30 -0400

    update relaed to alert

--- step-definitions/alertMessageOpenRequest/alertMessageOpenRequest.js
+++ step-definitions/alertMessageOpenRequest/alertMessageOpenRequest.js
@@ -59,8 +59,8 @@ When(/^user see the alerts available in the section with each having alert icon$
 
 Then(/^user see the alerts available in the section with each having alert title$/, function () {
 
-   dashboardPage.alertTitle.waitForExist(2000)
-   commonLib.assertValue(true, dashboardPage.alertTitle.isExisting()) 
+   dashboardPage.alertTitle1.waitForExist(2000)
+   commonLib.assertValue(true, dashboardPage.alertTitle1.isExisting()) 
    browser.pause(1000)
 
 });
@@ -82,20 +82,12 @@ When(/^user see the alerts available in the section with each of them having the
 });
 
 Then(/^user see the latest alert is displaying on the top$/, function () {
-
+   browser.pause(3000)
    var monthsName = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
 
    dashboardPage.alertTitle1.waitForExist(2000)
    commonLib.assertValue(true, dashboardPage.alertTitle1.isExisting())
-  //  var alertTitle1 =  dashboardPage.alertTitle1.getText()
-   //var alertTimestamp1 =  dashboardPage.alertTimestamp1.getText()
-
-
-
-
-
-   browser.pause(2000)
-
+   browser.pause(2000);
     var alertTimestamp1 =  dashboardPage.alertTimestamp1.getText()
     var alertTimestamp2 =  dashboardPage.alertTimestamp2.getText()
     var alertTimestamp3 =  dashboardPage.alertTimestamp3.getText()
@@ -107,7 +99,7 @@ Then(/^user see the latest alert is displaying on the top$/, function () {
     console.log("#################################")
     console.log(alertTimestamp3)
 
-    var timestamps = ['alertTimestamp1', 'alertTimestamp2', 'alertTimestamp3'];
+    var timestamps = [alertTimestamp1, alertTimestamp2, alertTimestamp3];
     var testSuccess = true;
     for(let i=0; i<timestamps.length; i++){
     if(!!timestamps[i] && !!timestamps[i+1] && testSuccess){
@@ -128,45 +120,60 @@ Then(/^user see the latest alert is displaying on the top$/, function () {
   }
 
   if(testSuccess){
-    console.log("test is successful")
-  }
+    console.log("test is successful")  }
+});
 
-    //var firstDate = "Jul 28";
-    // var firstDateSplit = alertTimestamp1.split(" ");
-    // var secondDateSplit = alertTimestamp2.split(" ");
+Given(/^user again navigates to message$/, function () {
+  
+  //dashboardPage.headerusageMenu.waitForExist(10000)
+  dashboardPage.alertarrowDown.click()
+  browser.pause(5000)
+  console.log("message arrow clicked")
+});
 
-    // var date1 = {"day":firstDateSplit[1],"monthIndex": monthsName.indexOf(firstDateSplit[0])};
+When(/^user see the messages available in the section with each of them having the alert icon$/, function () {
 
-    // var date2 = {"day":secondDateSplit[1],"monthIndex": monthsName.indexOf(secondDateSplit[0])};
-
-
-    // for(i=0; i<=3; i++){
-    // if(date1.day>=date2.day && date1.monthIndex>=date2.monthIndex){
-    //   console.log('First alert is grater than second');
-    // }else{
-    //   console.log('First alert is laser than second');      
-    // }
-    // }
-    // //var date2 =  dashboardPage.alertTimestamp1.getText()
-
-    // var compareDates = [];
-    // compareData.push(date1);
-
-
-    // if(date1.monthIndex >= date2.monthIndex) {
-  	// console.log("Date1 is greater than Date2");
-    // } else if(date1.monthIndex < date2.monthIndex) {
-	  // console.log("Date2 is greater than Date1");
-    // } else {
-    // if(date1.day >= date2.day) {
-	  // console.log("Date1 is greater than Date2");
-    // } else if(date1.day < date2.day) {
-    // console.log("Date2 is greater than Date1");
-    // }
-  // }
-
-  // for(var i = 0; i < compareData.length; i++){
-
-  // }
+  dashboardPage.alertIcon.waitForExist(4000)
+  commonLib.assertValue(true, dashboardPage.alertIcon.isExisting()) 
 
 });
+
+Then(/^user see the latest message is displaying on the top$/, function () {
+   browser.pause(3000)
+   var monthsName = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
+
+   dashboardPage.alertTitle1.waitForExist(2000)
+   commonLib.assertValue(true, dashboardPage.alertTitle1.isExisting())
+   browser.pause(2000);
+    var alertTimestamp1 =  dashboardPage.alertTimestamp1.getText()
+    var alertTimestamp2 =  dashboardPage.alertTimestamp2.getText()
+    var alertTimestamp3 =  dashboardPage.alertTimestamp3.getText()
+
+    console.log("#################################")
+    console.log(alertTimestamp1)
+    console.log("#################################")
+    console.log(alertTimestamp2)
+    console.log("#################################")
+    console.log(alertTimestamp3)
+
+    var timestamps = [alertTimestamp1, alertTimestamp2, alertTimestamp3];
+    var testSuccess = true;
+    for(let i=0; i<timestamps.length; i++){
+    if(!!timestamps[i] && !!timestamps[i+1] && testSuccess){
+        var firstDateSplit = timestamps[i].split(" ");
+        var secondDateSplit = timestamps[i+1].split(" ");
+
+        var date1 = {"day":firstDateSplit[1],"monthIndex": monthsName.indexOf(firstDateSplit[0])};
+
+        var date2 = {"day":secondDateSplit[1],"monthIndex": monthsName.indexOf(secondDateSplit[0])};
+        if(date1.day>=date2.day && date1.monthIndex>=date2.monthIndex){
+            // testSuccess = false;
+        }else{
+            testSuccess = false;
+        }
+    }
+  }
+  
+  if(testSuccess){
+    console.log("test is successful")  }
+  });
